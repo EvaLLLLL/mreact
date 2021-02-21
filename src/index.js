e5bd2a044c970c5e1eeff8cddbb591f7417f52a5
lyhhh <17610603221@163.com>
2021-02-21 23:24:41 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@
  * index
  */
 
-import "./test";
+import { mreact } from "./mreact";
+
+const domRoot = document.getElementById("root");
+
+const mInput = mreact.createElement("input");
+mreact.render(mInput, domRoot);
+
+/** @jsx mreact.createElement */
+const JSXElement = (
+  <div id="container">
+    <input value="foo" type="text" />
+    <span onClick={() => console.log("Hi")}>click me</span>
+  </div>
+);
+mreact.render(mInput, JSXElement);
